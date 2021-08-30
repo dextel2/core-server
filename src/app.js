70ef92bb12589adf257107c6a6c3e70a5ed7ab7d
Yash Karanke <dex.papa@gmail.com>
2021-08-31 01:06:37 +0530

    Added Static HTML 404

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@
  */
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 /**
  * ROUTES
  */
@@ -20,7 +21,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(1337);
--- src/routes/admins.js
+++ src/routes/admins.js
@@ -1,10 +1,10 @@
 const path = require("path");
 const express = require("express");
-
+const rootDir = require("../utils/path");
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/product", (req, res, next) => {

04e9a416c0af0ade90cc70574a04bd5e95c1274c
Yash Karanke <dex.papa@gmail.com>
2021-09-14 18:54:00 +0530

    working with ejs

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -6,6 +6,10 @@ const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/', productsController.getProducts);
+// /admin/add-product => GET
+router.get('/add-product', productsController.getAddProduct);
+
+// /admin/add-product => POST
+router.post('/add-product', productsController.postAddProduct);
 
 module.exports = router;

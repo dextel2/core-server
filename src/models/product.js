28f562e524b67a19d988a038bc2342da06fd11b3
Yash Karanke <dex.papa@gmail.com>
2021-09-14 21:01:40 +0530

    adjustments

--- src/models/product.js
+++ src/models/product.js
@@ -1,32 +1,40 @@
-const products = [];
-const fs = require("fs");
-const path = require("path");
-const p = path.join(path.dirname(process.mainModule.filename), "data", "products.json");
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
+      fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
     });
   }
+
   static fetchAll(cb) {
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      }
-      cb(JSON.parse(fileContent));
-    });
-    return products;
+    getProductsFromFile(cb);
   }
 };

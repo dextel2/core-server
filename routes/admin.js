773d97bd1e7844036464e080195106e18cb3312b
Yash Karanke <dex.papa@gmail.com>
2021-09-18 19:56:42 +0530

    reworked project structure

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,18 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const adminController = require('../controllers/admin');
+const adminController = require("../controllers/admin");
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', adminController.getAddProduct);
+router.get("/add-product", adminController.getAddProduct);
 
 // /admin/products => GET
-router.get('/products', adminController.getProducts);
+router.get("/products", adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', adminController.postAddProduct);
+router.post("/add-product", adminController.postAddProduct);
 
 module.exports = router;

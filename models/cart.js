679ef4df9b6f4a36dcbea23a76df7f3ae1b159d9
Yash Karanke <dex.papa@gmail.com>
2021-09-22 14:11:50 +0530

    fixed bug

--- models/cart.js
+++ models/cart.js
@@ -39,6 +39,9 @@ module.exports = class Cart {
       }
       const updatedCart = { ...JSON.parse(fileContent) };
       const product = updatedCart.products.find((prod) => prod.id === id);
+      if (!product) {
+        return;
+      }
       const productQty = product.qty;
       updatedCart.products = updatedCart.products.filter((prod) => prod.id !== id);
       updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
@@ -50,17 +53,6 @@ module.exports = class Cart {
   }
 
   static getCart(cb) {
-    fs.readFile(p, (err, fileContent) => {
-      const cart = JSON.parse(fileContent);
-      if (err) {
-        cb(null);
-      } else {
-        cb(cart);
-      }
-    });
-  }
-
-  static getProducts(cb) {
     fs.readFile(p, (err, fileContent) => {
       const cart = JSON.parse(fileContent);
       if (err) {

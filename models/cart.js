7be69bc7fbfa28e3e706ce5f263067a48fcf25b3
Yash Karanke <dex.papa@gmail.com>
2021-09-22 13:32:09 +0530

    Cart Items
    + Successfully adding cart items

--- models/cart.js
+++ models/cart.js
@@ -59,4 +59,15 @@ module.exports = class Cart {
       }
     });
   }
+
+  static getProducts(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }
+    });
+  }
 };

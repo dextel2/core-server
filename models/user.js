ae38aecf650acffbbbe909d0545c620a0dd8e27a
Yash Karanke <dex.papa@gmail.com>
2021-09-30 22:15:21 +0530

    Working on Cart

--- models/user.js
+++ models/user.js
@@ -4,29 +4,43 @@ const getDb = require("../utils/database").getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-    constructor(username, email) {
-        this.name = username;
-        this.email = email;
-    }
+	constructor(username, email, cart, id) {
+		this.name = username;
+		this.email = email;
+		this.cart = cart;
+		this._id = id;
+	}
 
-    save() {
-        const db = getDb();
-        return db.collection("users").insertOne(this);
-    }
+	save() {
+		const db = getDb();
+		return db.collection("users").insertOne(this);
+	}
 
-    static findById(userId) {
-        const db = getDb();
-        return db
-            .collection("users")
-            .find({ _id: new ObjectId(userId) })
-            .next()
-            .then((user) => {
-                return user;
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }
+	addToCart(product) {
+		// const cartProduct = this.cart.items.findIndex(cp=>{
+		// 	return cp._id === product._id
+		// })
+
+		const updatedCart = { items: [{ ...product, quantity: 1 }] };
+		const db = getDb();
+		return db
+			.collection("users")
+			.updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: { cart: updatedCart } });
+	}
+
+	static findById(userId) {
+		const db = getDb();
+		return db
+			.collection("users")
+			.find({ _id: new ObjectId(userId) })
+			.next()
+			.then((user) => {
+				return user;
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
 }
 
 module.exports = User;

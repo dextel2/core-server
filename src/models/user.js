a66bcb9e969c9cbc6e72dd4d919bb09e0c45b1b8
Yash Karanke <16697307+dextel2@users.noreply.github.com>
2022-08-07 13:09:38 +0530

    Copilot
    Added code regards to co-point, needs review

--- src/models/user.js
+++ src/models/user.js
@@ -11,68 +11,110 @@ class User {
 		this._id = id;
 	}
 
+	login(email, password) {
+		const db = getDb();
+		return db
+			.collection("users")
+			.findOne({ email: email })
+			.then((user) => {
+				if (!user) {
+					return Promise.reject("User not found");
+				}
+				return bcrypt.compare(password, user.password).then((res) => {
+					if (res) {
+						return Promise.resolve(user);
+					}
+					return Promise.reject("Wrong password");
+				});
+			});
+	}
+
 	save() {
 		const db = getDb();
 		return db.collection("users").insertOne(this);
 	}
 
-	addToCart(product) {
-		const cartProductIndex = this.cart.items.findIndex((cp) => {
-			return cp.productId.toString() === product._id.toString();
-		});
-
-		let newQuantity = 1;
-		const updatedCartItems = [...this.cart.items];
-
-		if (cartProductIndex >= 0) {
-			newQuantity = this.cart.items[cartProductIndex].quantity + 1;
-			updatedCartItems[cartProductIndex].quantity = newQuantity;
-		} else {
-			updatedCartItems.push({
-				productId: new ObjectId(product._id),
-				quantity: newQuantity,
-			});
-		}
-		const updatedCart = {
-			items: updatedCartItems,
-		};
+	/**
+	 * Create register api
+	 */
+	static register(username, email, password) {
 		const db = getDb();
-		return db.collection("users").updateOne({ _id: new ObjectId(this._id) }, { $set: { cart: updatedCart } });
-	}
-
-	getCart() {
-		const db = getDb();
-		const productIds = this.cart.items.map((el) => {
-			return el.productId;
-		});
+		const user = new User(username, email, [], new ObjectId());
 		return db
-			.collection("products")
-			.find({ _id: { $in: productIds } })
-			.toArray()
-			.then((products) => {
-				return products.map((product) => {
-					return {
-						...product,
-						quantity: this.cart.items.find((i) => {
-							return i.productId.toString() === product._id.toString();
-						}).quantity,
-					};
+			.collection("users")
+			.findOne({ email: email })
+			.then((user) => {
+				if (user) {
+					return Promise.reject("Email already exists");
+				}
+				return bcrypt.hash(password, 12).then((hash) => {
+					user.password = hash;
+					return db.collection("users").insertOne(user);
+				}).then(() => {
+					return user;
 				});
 			});
 	}
-	static findById(userId) {
-		const db = getDb();
-		return db
-			.collection("users")
-			.find({ _id: new ObjectId(userId) })
-			.next()
-			.then((user) => {
-				return user;
-			})
-			.catch((err) => {
-				console.log(err);
-			});
+}
+
+
+addToCart(product) {
+	const cartProductIndex = this.cart.items.findIndex((cp) => {
+		return cp.productId.toString() === product._id.toString();
+	});
+
+	let newQuantity = 1;
+	const updatedCartItems = [...this.cart.items];
+
+	if (cartProductIndex >= 0) {
+		newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+		updatedCartItems[cartProductIndex].quantity = newQuantity;
+	} else {
+		updatedCartItems.push({
+			productId: new ObjectId(product._id),
+			quantity: newQuantity,
+		});
 	}
+	const updatedCart = {
+		items: updatedCartItems,
+	};
+	const db = getDb();
+	return db.collection("users").updateOne({ _id: new ObjectId(this._id) }, { $set: { cart: updatedCart } });
+}
+
+getCart() {
+	const db = getDb();
+	const productIds = this.cart.items.map((el) => {
+		return el.productId;
+	});
+	return db
+		.collection("products")
+		.find({ _id: { $in: productIds } })
+		.toArray()
+		.then((products) => {
+			return products.map((product) => {
+				return {
+					...product,
+					quantity: this.cart.items.find((i) => {
+						return i.productId.toString() === product._id.toString();
+					}).quantity,
+				};
+			});
+		});
+}
+	static findById(userId) {
+	const db = getDb();
+	return db
+		.collection("users")
+		.find({ _id: new ObjectId(userId) })
+		.next()
+		.then((user) => {
+			return user;
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+}
 }
 
 module.exports = User;

bd31f0322a70151899577f3dae9a7b1285a0c5d0
Yash Karanke <yashk@nuv.ac.in>
2022-10-02 19:50:48 +0530

    Formatted document

--- src/controllers/error.js
+++ src/controllers/error.js
@@ -1,5 +1,5 @@
 const get404 = (req, res, next) => {
-	res.status(404).render("404", { pageTitle: "Page Not Found", path: "/404" });
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: "/404" });
 };
 
 export { get404 };

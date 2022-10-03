64614a424d5ab66434f6706cf736717e283f9883
Yash Karanke <yashk@nuv.ac.in>
2022-10-03 13:00:26 +0530

    #13 code review
    + Changed normal function to named function, It's ideal to use arrow function instead of conventional function if we are not returning anything

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -2,15 +2,15 @@ const backdrop = document.querySelector(".backdrop");
 const sideDrawer = document.querySelector(".mobile-nav");
 const menuToggle = document.querySelector("#side-menu-toggle");
 
-function backdropClickHandler() {
-    backdrop.style.display = "none";
-    sideDrawer.classList.remove("open");
-}
+const backdropClickHandler = () => {
+  backdrop.style.display = "none";
+  sideDrawer.classList.remove("open");
+};
 
-function menuToggleClickHandler() {
-    backdrop.style.display = "block";
-    sideDrawer.classList.add("open");
-}
+const menuToggleClickHandler = () => {
+  backdrop.style.display = "block";
+  sideDrawer.classList.add("open");
+};
 
 backdrop.addEventListener("click", backdropClickHandler);
 menuToggle.addEventListener("click", menuToggleClickHandler);

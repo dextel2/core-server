b26d588c2cd3383ed67146ff905b2a2a2aa5ae85
Yash Karanke <16697307+dextel2@users.noreply.github.com>
2023-10-02 16:15:15 +0530

    add get404 controller

--- server.js
+++ server.js
@@ -4,15 +4,14 @@ import fs from "fs";
 import multer from "multer";
 import path from "path";
 import sharp from "sharp";
-import { 
-} from "./src/controllers/error";
+import { get404 } from "./src/controllers/error";
 import User from "./src/models/user";
 import adminRoutes from "./src/routes/admin";
 import shopRoutes from "./src/routes/shop";
 import { mongoConnect } from "./src/utils/database";
 const storage = multer.memoryStorage();
 const uploads = multer({ storage });
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 1337;
 const app = express();
 
 app.set("view engine", "ejs");
@@ -22,34 +21,34 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "/src/public")));
 
 app.use((req, res, next) => {
-	User.findById("615490961edaa81fdda5f2c7")
-		.then((user) => {
-			req.user = new User(user.name, user.email, user.cart, user._id);
-			next();
-		})
-		.catch((err) => console.log(err));
+  User.findById("615490961edaa81fdda5f2c7")
+    .then((user) => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch((err) => console.log(err));
 });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.post("/api/v1/images", uploads.single("thumbnail"), async (req, res) => {
-	console.log("file", req.file);
-	console.log("body", req.body);
-	fs.access("./src/data/uploads/", (err) => {
-		if (err) {
-			fs.mkdirSync("./data/uploads");
-		}
-	});
-	await sharp(req.file.buffer)
-		.resize({ width: 650, height: 350 })
-		.toFile("./src/data/uploads/" + req.file.originalname);
-	res.send("success");
+  console.log("file", req.file);
+  console.log("body", req.body);
+  fs.access("./src/data/uploads/", (err) => {
+    if (err) {
+      fs.mkdirSync("./data/uploads");
+    }
+  });
+  await sharp(req.file.buffer)
+    .resize({ width: 650, height: 350 })
+    .toFile("./src/data/uploads/" + req.file.originalname);
+  res.send("success");
 });
 
 app.use(get404);
 
 mongoConnect(() => {
-	app.listen(port, () => {
-		console.log(`server is running on ${port}`);
-	});
+  app.listen(port, () => {
+    console.log(`server is running on ${port}`);
+  });
 });
--- src/controllers/error.js
+++ src/controllers/error.js
@@ -1,5 +1,4 @@
-const get404 = (req, res, next) => {
+export const get404 = (req, res, next) => {
   res.status(404).render("404", { pageTitle: "Page Not Found", path: "/404" });
 };
 
-export { get404 };
